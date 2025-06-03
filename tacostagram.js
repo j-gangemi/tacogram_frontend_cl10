2df0d2f8cfcb45c44c07a27b08761ff6928472dd
Julia Gangemi <julia.gangemi@kellogg.northwestern.edu>
2025-06-03 16:28:19 +0000

    class changes

--- tacostagram.js
+++ tacostagram.js
@@ -1,6 +1,6 @@
 function getPostsFromAPI() {
   // Replace this with the URL of the JSON API that returns an array of image URLs
-  const url = 'YOUR_URL_GOES_HERE';
+  const url = 'https://3000-jgangemi-tacogramfinalc-6k4l2igj8c5.ws-us120.gitpod.io/posts.json';
   if (url == 'YOUR_URL_GOES_HERE') {
     alert('Error: Replace url value in tacostagram.js')
   }
